af74ec375cd34f9b3c4720d6ad1412e3ef0af4bc
pkboe <pranilkharche@gmail.com>
2020-09-07 19:17:50 +0530

    Updates

---
+++ precache-manifest.f1a5f58129c06015bd54607acd732b0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "589ddf2a14b4f5c8c95acf2c495ee478",
+    "url": "/covid-react/index.html"
+  },
+  {
+    "revision": "5da67f421120e7401e8e",
+    "url": "/covid-react/static/css/main.5ee58117.chunk.css"
+  },
+  {
+    "revision": "093bee15426b52e0a2a4",
+    "url": "/covid-react/static/js/2.5e47f140.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/covid-react/static/js/2.5e47f140.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5da67f421120e7401e8e",
+    "url": "/covid-react/static/js/main.2e662090.chunk.js"
+  },
+  {
+    "revision": "fe05d39d5b78a1d36a01",
+    "url": "/covid-react/static/js/runtime-main.9f1a86d6.js"
+  }
+]);
